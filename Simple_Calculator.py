19bfbf17e1d6b07143152da47eef10608e0ece4d
luketeran <s10222582@connect.np.edu.sg>
2023-10-28 20:42:27 +0800

    feat: completed requirements for Percentage and Square Root

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -10,10 +10,67 @@ def exponents():
     pass
 
 def square_root():
-    pass
+    while(True):
+        option = sqrt_menu()
+        if(option == '0'):
+            break
+        else:
+            print('Value to Square Root')
+            try:
+                value = float(input('>'))
+                print()
+                print('Square Root Value is \t')
+                print(math.sqrt(value))
 
-def percentage():
-    pass
+            except ValueError:
+                print('Please Enter a number:')
+
+def sqrt_menu():#menu to display when the option for the square root function is selected. returns a value that represents the option
+    print('\nChoose an Option: ')
+    print('1. Input')
+    print('0. Exit')
+    option = input(">")
+    return option
+
+def getPercentage():
+    try:
+        value = float(input('Enter Value: \n>'))
+        totalValue = float(input('Enter Total Value: \n>'))
+
+    except ValueError:
+        print('Please Enter a number')
+        print('===================================')
+    percentage = (value / totalValue) * 100
+    print('Percentage Value is ', percentage)
+
+def getValue():
+    try:
+        totalValue = float(input('Enter Total Value: \n'))
+        percentage = float(input('Enter Percentage value: \n'))
+
+    except ValueError:
+        print('Please Enter a number')
+        print('===================================')
+
+    value = (percentage /100) * totalValue
+    print('Value is ', value)
+
+def percentage_Menu():#menu to display when the option for the percentage function is selected. returns a value that represents the option
+    while(True):
+        print('\nChoose an Option: ')
+        print('1. Find Percentage')
+        print('2. Find Value of a Percentage')
+        print('0. Exit')
+        option = input(">")
+
+        if (option == '1'):
+            getValue()
+
+        elif(option == '2'):
+            getPercentage()
+
+        else:
+            break
 
 def main_menu():
     print('\nChoose a category of operation:')
@@ -41,7 +98,7 @@ def main():
             square_root()
             break
         elif category == '5':
-            percentage()
+            percentage_Menu()
             break
         elif category == '6':
             return 'exit'
